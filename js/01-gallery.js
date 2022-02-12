dd6eb45e2db77caec27dc2b8c5880a1e1712adc4
Roman Stankevych <romandionus@gmail.com>
2022-02-12 16:19:26 +0200

    1. step

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(`div`);
+const newLinkImgEl = galleryItems.map((item) => `<a class="gallery__link" href="${item.original}" target="_self">
+<img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a>`).join("")
+galleryEl.insertAdjacentHTML(`beforebegin`, newLinkImgEl)
+
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
